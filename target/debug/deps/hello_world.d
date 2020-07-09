923a462f63e0017ef3abc7963fc2cde2f1583594
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2020-07-09 10:14:28 +0100

    Add gitignore

--- target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-/Users/ppamorim/Repository/mainframer-test/target/debug/deps/hello_world: src/main.rs
-
-/Users/ppamorim/Repository/mainframer-test/target/debug/deps/hello_world.d: src/main.rs
-
-src/main.rs:
--- target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/ppamorim/Repository/mainframer-test/target/debug/hello_world: /Users/ppamorim/Repository/mainframer-test/src/main.rs
